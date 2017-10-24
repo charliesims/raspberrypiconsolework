ff61f68c0355c03fd69cdaf76f14558924d4233f
charliesims <32739619+charliesims@users.noreply.github.com>
2017-10-24 10:03:24 -0400

    Add files via upload

---
+++ Execute.cpp
@@ -0,0 +1,43 @@
+rootdir="/opt/retropie"
+
+
+if [[ -n "$__user" ]]; then
+    user="$__user"
+    if ! id -u "$__user" &>/dev/null; then
+        echo "User $__user not exist"
+        exit 1
+    fi
+else
+    user="$SUDO_USER"
+    [[ -z "$user" ]] && user="$(id -un)"
+fi
+
+home="$(eval echo ~$user)"
+datadir="$home/RetroPie"
+biosdir="$datadir/BIOS"
+romdir="$datadir/roms"
+emudir="$rootdir/emulators"
+configdir="$rootdir/configs"
+
+scriptdir="$(dirname "$0")"
+scriptdir="$(cd "$scriptdir" && pwd)"
+
+__logdir="$scriptdir/logs"
+__tmpdir="$scriptdir/tmp"
+__builddir="$__tmpdir/build"
+__swapdir="$__tmpdir"
+
+
+if [[ "$(id -u)" -ne 0 ]]; then
+    echo "Script must be run under sudo from the user you want to install for. Try 'sudo $0'"
+    exit 1
+fi
+
+__backtitle="retropie.org.uk - RetroPie Setup. Installation folder: $rootdir for user $user"
+
+source "$scriptdir/scriptmodules/system.sh"
+source "$scriptdir/scriptmodules/helpers.sh"
+source "$scriptdir/scriptmodules/inifuncs.sh"
+source "$scriptdir/scriptmodules/packages.sh"
+
+
